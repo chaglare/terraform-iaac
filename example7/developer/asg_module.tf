b3d45868ae1b86e1c5905ceeb7a43698806551d9
chaglare <chaglare@gmail.com>
2020-12-10 04:49:41 +0000

    edited

--- example7/developer/asg_module.tf
+++ example7/developer/asg_module.tf
@@ -1,9 +1,10 @@
 module "webapp" {
   source           = "../"
   region           = "us-east-1"
-  max_size         = "1"
-  min_size         = "1"
-  desired_capacity = "1"
+  max_size         = "2"
+  min_size         = "2"
+  desired_capacity = "2"
   image_owner      = "137112412989"
   instance_type    = "c5.large"
+  Env              = "Dev"
 }
--- example7/developer/backend.tf
+++ example7/developer/backend.tf
@@ -1,5 +1,6 @@
 terraform {
-    required_version = "0.11.14"
+  required_version = "0.11.14"
+
   backend "s3" {
     bucket = "terraform-chaglare"
     key    = "terraform.tfstate"
