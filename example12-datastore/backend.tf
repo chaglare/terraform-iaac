5c3d89eaedd1d94b10a32b14a58c8a0d8ad9d3f5
chaglare <chaglare@gmail.com>
2021-01-09 05:11:29 +0000

    data store added

--- example12-datastore/backend.tf
+++ example12-datastore/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ADD YOUR S3 BUCKET HERE"
+    bucket = "ADD YOUR S3 BUCKET HERE" # Uncommend backend.tf in .gitignore if you push your code to SM
     key    = "path/to/my/remote_state"
     region = "us-east-1"
   }
--- example12-datastore/test/data.tf
+++ example12-datastore/test/data.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "main" {
   backend = "s3"
 
   config = {
-    bucket = "ADD YOUR S3 BUCKET HERE"
+    bucket = "ADD YOUR S3 BUCKET HERE" # Uncommend backend.tf in .gitignore if you push your code to SM
     key    = "path/to/my/remote_state"
     region = "us-east-1"
   }
